e9cc1b22babb4393f0d083eee3ff2f71db6d6664
Pierre <pgadea@users.noreply.github.com>
2019-12-08 22:26:37 -0600

    Add token to header, updated useFetch, created backendErrorMessage and currentUserChecker

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import {BrowserRouter as Router} from 'react-router-dom';
 
 import Routes from 'routes';
 import Topbar from 'components/topbar';
+import {CurrentUserProvider} from 'contexts/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
 
 const App = () => {
     return (
+      <CurrentUserProvider>
+        <CurrentUserChecker>
             <Router>
                 <Topbar />
                 <Routes />
             </Router>
+        </CurrentUserChecker>
+      </CurrentUserProvider>
     )
   }
 
